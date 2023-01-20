da9de26753c8d9545ca222f6d28f1ebb2c9d4ea8
geekyweekend9pm <112350734+geekyweekend9pm@users.noreply.github.com>
2023-01-20 21:23:03 +0000

    Initial commit

---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import "header.css";
+export default function Header (){
+return (
+<header classsName="header" >
+<p className="header-content">header </p>
+
+</header>
+
+
+)
+}
